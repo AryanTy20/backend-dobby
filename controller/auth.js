546d71e8e4504005251dd9e54e40546b0f100698
AryanTy20 <devty007@gmail.com>
2022-11-03 15:10:17 +0530

    fix: export and import

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,11 @@
+import { User } from "../models";
+import bcrypt from "bcrypt";
+import { CustomError } from "../utils";
+
 export const AuthController = {
-  async signUp(req, res, next) {},
+  async signUp(req, res, next) {
+    res.json(req.body);
+  },
   async signIn(req, res, next) {},
   async logout(req, res, next) {
     res.send("logout");
--- utils/error.js
+++ utils/error.js
@@ -1,6 +1,8 @@
-export const CustomError = (status, message) => {
+const CustomError = (status, message) => {
   const err = new Error();
   err.status = status;
   err.message = message;
   return err;
 };
+
+export default CustomError;
