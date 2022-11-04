5da8862e69d252d46988f598e025cd0a43cb0754
AryanTy20 <devty007@gmail.com>
2022-11-04 07:58:10 +0530

    feat: protect user route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
 import { UserController as UC } from "../controller";
+import { verifyToken } from "../middleware/verifyToken";
 
 // signup
-router.post("/upload", UC.upload);
-router.get("/search", UC.search);
-router.get("/", UC.getImg);
+router.post("/upload", verifyToken, UC.upload);
+router.get("/search", verifyToken, UC.search);
+router.get("/", verifyToken, UC.getImg);
 
 export default router;
