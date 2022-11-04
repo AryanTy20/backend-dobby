dfdb324701f64760c7381fed9af9058258ed23fc
AryanTy20 <devty007@gmail.com>
2022-11-04 07:56:33 +0530

    feat: token verification before logout and refresh token access

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 const router = Router();
 import { AuthController as Auth } from "../controller";
-
+import { verifyToken } from "../middleware/verifyToken";
 // signup
 router.post("/register", Auth.signUp);
 router.post("/login", Auth.signIn);
-router.get("/logout", Auth.logout);
-router.get("/refreshuser", Auth.refreshAccess);
+router.get("/logout", verifyToken, Auth.logout);
+router.get("/refreshuser", verifyToken, Auth.refreshAccess);
 
 export default router;
