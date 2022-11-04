3ec21f9d7440306a28fa963c0acd1646ba0cd557
AryanTy20 <devty007@gmail.com>
2022-11-04 07:54:17 +0530

    feat: verifyToken middleware created

---
+++ middleware/verifyToken.js
@@ -0,0 +1,16 @@
+import { JWT, CustomError } from "../utils";
+
+export const verifyToken = (req, res, next) => {
+  if (!req.cookies) return next(CustomError(403, "Unauthorized"));
+  const id = JWT.signVerify(req.cookies.token);
+  if (!id) {
+    res
+      .clearCookie("token", {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      })
+      .json("Token expire");
+  }
+  req.user.id = id;
+  next();
+};
