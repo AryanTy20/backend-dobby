b2b4477afbd325cd41ce369309a260e2b3fb22ef
Aryan Tirkey <65565216+AryanTy20@users.noreply.github.com>
2022-11-05 17:36:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 const PORT = process.env.PORT || 5000;
 //core setting
-const allowedUrl = ["https://shiny-squirrel-26dd3f.netlify.app"];
+const allowedUrl = ["https://shiny-squirrel-26dd3f.netlify.app,http://localhost:3000"];
 app.use(express.json({ limit: "5mb", extended: true }));
 app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
@@ -20,6 +20,10 @@ dbConnect();
 
 //Routes
 import { AuthRoutes, UserRoutes } from "./routes";
+app.get("/",(req,res)=>{
+  res.json("server running api")
+});
+
 app.use("/api/auth", AuthRoutes);
 app.use("/api/user", UserRoutes);
 
